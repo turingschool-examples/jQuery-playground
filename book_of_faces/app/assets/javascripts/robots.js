b93072e5282d1e109d768a368b9f74beacd9cf09
Rachel Warbelow <rachelwarbelow@gmail.com>
2015-03-17 10:04:20 -0600

    implemented

--- book_of_faces/app/assets/javascripts/robots.js
+++ book_of_faces/app/assets/javascripts/robots.js
@@ -14,4 +14,28 @@ $(document).ready(function () {
     });
   });
 
+  $('#robot_filter_state').on('change', function () {
+    var currentState = this.value;
+    $robots.each(function (index, robot) {
+      $robot = $(robot);
+      if ($robot.data('state') === currentState) {
+        $robot.show();
+      } else {
+        $robot.hide();
+      }
+    });
+  });
+
+  $('#robot_filter_name').on('keyup', function() {
+    var currentName = this.value;
+
+    $robots.each(function (index, robot){
+      $robot = $(robot);
+      if ($robot.data('name').indexOf(currentName) !== -1) {
+        $robot.show();
+      } else {
+        $robot.hide();
+      }
+    });
+  });
 });
